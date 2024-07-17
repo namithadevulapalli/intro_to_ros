f205a6644f788a663d171057ce6728dceaf03dd7
namitha d <namitha.devulapalli@gmail.com>
2024-07-17 10:33:46 -0400

    added

--- intro_to_ros/bluerov2_sensors.py
+++ intro_to_ros/bluerov2_sensors.py
@@ -1,7 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import BatteryState
-from sensor_msgs.msg import Imu
+from sensor_msgs.msg import BatteryState, Imu, FluidPressure
 from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy, QoSDurabilityPolicy
 
 class SensorSubscriber(Node):
@@ -11,6 +10,7 @@ class SensorSubscriber(Node):
         self.battery_state = None
         self.imu_state = None
 
+        # battery sensor subscriber
         self.battery_subscriber = self.create_subscription(
             BatteryState,
             "/mavros/battery",
@@ -24,6 +24,7 @@ class SensorSubscriber(Node):
         )
         self.battery_subscriber
 
+        # imu data subscriber
         self.imu_subscriber = self.create_subscription(
             Imu,
             "/mavros/imu/data",
@@ -37,25 +38,69 @@ class SensorSubscriber(Node):
         )
         self.imu_subscriber
 
-    
+        # differential pressure subscriber
+        self.pressure_diff_subscriber = self.create_subscription(
+            FluidPressure,
+            "mavros/imu/diff_pressure",
+            self.pressure_diff_callback,
+            qos_profile = QoSProfile(
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=10,
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            durability=QoSDurabilityPolicy.VOLATILE
+        )
+        )
 
+        self.pressure_diff_subscriber
+
+        # static pressure subscriber
+        self.pressure_static_subscriber = self.create_subscription(
+            FluidPressure,
+            "mavros/imu/static_pressure",
+            self.pressure_static_callback,
+            qos_profile = QoSProfile(
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=10,
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            durability=QoSDurabilityPolicy.VOLATILE)
+        )
+        self.pressure_static_subscriber
         
-
+        # timed volt checks
         self.volt_checker = self.create_timer(
             5.0, self.check_voltage
         )
 
         self.get_logger().info("starting subscriber nodes")
 
+    def pressure_diff_callback(self, msg):
+        '''takes sensor data for differential pressure and returns fluid pressure and variance'''
+        self.get_logger().info("diff fluid pressure: " + str(msg.fluid_pressure))
+        self.get_logger().info("diff variance: " + str(msg.variance))
+        
+
+    def pressure_static_callback(self, msg):
+        '''takes sensor data for static pressure and returns fluid pressure and variance'''
+        self.get_logger().info("static fluid pressure: " + str(msg.fluid_pressure))
+        self.get_logger().info("static variance: " + str(msg.variance))
+    
     def battery_callback(self, msg):
+        '''stores battery information in class variable'''
         self.battery_state = msg
 
     def imu_callback(self, msg):
+        '''stores IMU information in class variable'''
         self.imu_state = msg
 
+    def print_imu_data(self):
+        '''prints IMU data'''
+        self.get_logger().info(str(self.imu_state))
+
     def check_voltage(self):
-        self.get_logger().info(str(self.battery_state.voltage))
-        if self.battery_state.voltage < 3.0:
+        '''prints current voltage of battery and prints warning if it goes below 12V'''
+        self.get_logger().info("current voltage: " + str(self.battery_state.voltage))
+
+        if self.battery_state.voltage < 12.0:
             self.get_logger().info("battery voltage unsafe")
 
 
